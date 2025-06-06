94ef1d4b95089ee0accd69b3c1befa3cd77e206d
endranio <endranio576@gmail.com>
2025-06-06 22:16:32 +0700

    create endpoint POST

---
+++ database/seed.go
@@ -0,0 +1,26 @@
+package database
+
+import (
+	"log"
+	"shortsy/models"
+	"gorm.io/gorm"
+)
+
+func Seed(db*gorm.DB){
+	shorts := []model.Shortsy{
+		{OriginalURL: "https://google.com", ShortCode: "ggl"},
+		{OriginalURL: "https://github.com", ShortCode: "ghub"},
+	}
+	 for _, s := range shorts {
+       
+        var existing model.Shortsy
+        err := db.Where("short_code = ?", s.ShortCode).First(&existing).Error
+        if err == gorm.ErrRecordNotFound {
+       
+            err := db.Create(&s).Error
+            if err != nil {
+                log.Println("Seeding fail:", err)
+            }
+        }
+    }
+}
