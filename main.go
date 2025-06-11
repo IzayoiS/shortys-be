6c8a4cf908b0d3c14d924c2175de85a12c3bb8dd
IzayoiS <iqbalmhasby54@gmail.com>
2025-06-11 11:36:12 +0700

    fix: load env

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"log"
 	"os"
 
-	"shortsy/config"
 	"shortsy/database"
 	"shortsy/routes"
 
@@ -12,7 +11,6 @@ import (
 )
 
 func main() {
-	config.LoadEnv()
 	app := fiber.New()
 	database.Connect()
 	database.Seed(database.DB)
