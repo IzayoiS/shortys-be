86a78cc909ccff441e622eba882bd8fb0726def2
IzayoiS <iqbalmhasby54@gmail.com>
2025-06-12 14:31:36 +0700

    test with hard code url db

--- database/database.go
+++ database/database.go
@@ -2,7 +2,6 @@ package database
 
 import (
 	"log"
-	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -14,7 +13,7 @@ var DB *gorm.DB
 
 func Connect() {
 	
-	dsn := os.Getenv("DATABASE_URL")
+	dsn := "postgresql://postgres:jTdHHWEMMvl9Cq78@db.brmuhskfoxpfsiegzyyf.supabase.co:5432/postgres"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect database")
