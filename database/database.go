bde850382cc54b3719bfcbfba0e9c1b64777cd86
IzayoiS <iqbalmhasby54@gmail.com>
2025-06-13 10:19:51 +0700

    fix url api key

--- database/database.go
+++ database/database.go
@@ -1,24 +1,38 @@
 package database
 
 import (
-	"log"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
 
-	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-
-	model "shortsy/models"
 )
+
+var supabaseUrl = os.Getenv("SUPABASE_URL")
+var supabaseKey = os.Getenv("SUPABASE_ANON_KEY")
 
 var DB *gorm.DB
 
-func Connect() {
-	
-	db, err := gorm.Open(postgres.Open("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRwcHZ6c2pzdndibWRkZXd0bHh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkxMzU3MjksImV4cCI6MjA2NDcxMTcyOX0.VnMrcMNgHCZs8uCp8FHnvq0RQAi4HZqn-E4mjd_oHhI"), &gorm.Config{})
+func Connect() ([]map[string]interface{}, error) {
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/rest/v1/shortsy", supabaseUrl), nil)
 	if err != nil {
-		log.Fatal("Failed to connect database")
+		return nil, err
 	}
+	req.Header.Set("apikey", supabaseKey)
 
-	DB = db
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
 
-	db.AutoMigrate(&model.Shortsy{})
+	body, _ := ioutil.ReadAll(res.Body)
+
+	var result []map[string]interface{}
+	json.Unmarshal(body, &result)
+
+	return result, nil
 }
