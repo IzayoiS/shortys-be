42127383b73bddc78093b56d4514f48d112dd416
IzayoiS <iqbalmhasby54@gmail.com>
2025-06-13 10:05:32 +0700

    fix url with api

--- database/database.go
+++ database/database.go
@@ -13,7 +13,7 @@ var DB *gorm.DB
 
 func Connect() {
 	
-	db, err := gorm.Open(postgres.Open("postgresql://postgres:3RVwLUISRcri25w6@db.jgsyvsxijotyuoxxogsa.supabase.co:5432/postgres"), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRwcHZ6c2pzdndibWRkZXd0bHh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkxMzU3MjksImV4cCI6MjA2NDcxMTcyOX0.VnMrcMNgHCZs8uCp8FHnvq0RQAi4HZqn-E4mjd_oHhI"), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect database")
 	}
